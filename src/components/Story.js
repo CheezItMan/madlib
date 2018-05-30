b06a3765fa93fa9ca4a1e7af3d228b63bc6bbe43
Charles Ellis <hamled@hamled.name>
2018-05-29 17:46:42 -0700

    Story component renders madlib from text and subs

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import './Story.css';
 
 class Story extends Component {
   render() {
+    const madLib = this.buildMadLib();
+    const text = madLib(this.props.substitutions);
+
     return (
       <section className="story">
-        <p>{ this.props.text }</p>
+        <p>{text}</p>
       </section>
     );
+  }
+
+  buildMadLib() {
+    // Dynamically create a function which takes
+    // an object with word substitutions, and returns
+    // the current madlib with those substitutions filled-in
+    return Function('substitutions', `
+      let completeSubs = {};
+      this.props.madLib.keys.forEach((key) => {
+        completeSubs[key] = '_'.repeat(8);
+      });
+      completeSubs = { ...completeSubs, ...substitutions };
+
+      with(completeSubs) {
+        return this.reactMadLib\`${this.props.madLib.text}\`;
+      }
+    `).bind(this);
+  }
+
+  reactMadLib = (strings, ...subs) => {
+    let parts = [strings[0]];
+
+    subs.forEach((sub, i) => {
+      parts.push(<span className='story-substitution'>{sub}</span>, strings[i + 1]);
+    });
+
+    return parts;
   }
 }
 
